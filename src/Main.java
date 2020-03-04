11a9cee839bb4b7387c59b83e6543d5213964bd8
BrestenskyMW1 <44762439+BrestenskyMW1@users.noreply.github.com>
2020-03-04 15:13:47 -0500

    Timing code
    Added code to main to time and run algorithm

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,39 @@
+import java.util.Random;
 
 public class Main {
+	
+	public static int gridSize = 7;
+			
 	public static void main(String[]args) {
-		System.out.println("Lights out for you!");
+		
+		//create a grid to run through the algorithm
+		boolean[][] startGrid = new boolean[gridSize][gridSize];
+		Random rnd = new Random();
+		
+		//initialize startGrid
+		for(int i = 0; i < gridSize; i++) {
+			for(int j = 0; j < gridSize; j++) {
+				int checkNum = rnd.nextInt() % 5;
+				if(checkNum < 2)
+					startGrid[i][j] = true;
+				else
+					startGrid[i][j] = true;
+			}
+		}
+		
+		//start timer
+		long startTime = System.currentTimeMillis();
+		
+		//run algorithm
+		LightsOut.solveGrid(startGrid);
+		
+		//stop timer
+		long endTime = System.currentTimeMillis();
+		long runtime = endTime - startTime;
+		
+		//print timer results
+		System.out.println(runtime + " milliseconds");
+		long seconds = runtime/1000;
+		System.out.println(seconds + " seconds");
 	}
 }
