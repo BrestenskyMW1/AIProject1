2221ac193ad86947aafede23f2a31497dc35dcfc
BrestenskyMW1 <44762439+BrestenskyMW1@users.noreply.github.com>
2020-03-05 13:37:25 -0500

    UI Begins
    Created a new class UIPriject1 for our JavaFx UI needs. This could at the very least be a testing ground, but in theory we could move all the UI stuff to class Main.

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,7 @@ public class Main {
 	
 	public static int gridSize = 5;
 			
-	public static void main(String[]args) throws FileNotFoundException {
+	public static void runTests() throws FileNotFoundException {
 		
 		//create a grid to run through the algorithm
 		boolean[][] startGrid;// = new boolean[gridSize][gridSize];
